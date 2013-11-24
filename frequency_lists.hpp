e5e27fecd8c2e06898655e1c94b0c25343316790
Péter Budai <buc@peterbudai.eu>
2013-11-24 15:07:23 +0100

    Created VS2012 project. Added data and scripts from the original zxcvbn library. Modified build_frequency_list to produce CPP output.

---
+++ frequency_lists.hpp
@@ -0,0 +1,20 @@
+#ifndef FREQUENCY_LISTS_HPP
+#define FREQUENCY_LISTS_HPP
+
+extern const char* passwords[];
+extern const size_t passwords_count;
+
+extern const char* male_names[];
+extern const size_t male_names_count;
+
+extern const char* female_names[];
+extern const size_t female_names_count;
+
+extern const char* surnames[];
+extern const size_t surnames_count;
+
+extern const char* english[];
+extern const size_t english_count;
+
+
+#endif
